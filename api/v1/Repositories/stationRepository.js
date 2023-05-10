95ef024a69acd88b5000845aba69ece99ef4f06b
Scleepy <danielyohanes03@gmail.com>
2023-05-10 22:38:38 +0700

    Daily Quest + Station Fix

--- api/v1/Repositories/stationRepository.js
+++ api/v1/Repositories/stationRepository.js
@@ -4,7 +4,7 @@ const sql = require('./databaseConnection');
 
 const getSpecificStation = async (stationID) => {
     try {
-        const result = await sql.query(`SELECT * FROM MsRecycleStation JOIN MsBuilding ON MsRecycleStation.BuildingID = MsBuilding.BuildingID WHERE StationID = '${stationID}'`);
+        const result = await sql.query(`SELECT StationID, StationLocation, BuildingName FROM MsRecycleStation JOIN MsBuilding ON MsRecycleStation.BuildingID = MsBuilding.BuildingID WHERE StationID = '${stationID}'`);
         return result.recordset;
     }catch(err){
         throw { status: 500, message: err };
